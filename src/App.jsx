b30a5054a9ae8c8663240b1e747c923341c39649
tradesecurity <kjrkjy@naver.com>
2025-04-05 20:36:50 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import SurveyForm from './SurveyForm';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <SurveyForm />
+    <div className="survey-container">
+      <h1>✨ BEAUTY CODE AI 설문지</h1>
+      {/* 설문 내용이 여기에 들어갑니다 */}
+      <button>피부 진단 제출</button>
     </div>
   );
-}
+};
 
 export default App;
